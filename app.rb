be9b1e8f5262e0728aa34c1d8750928ff9fb324e
Yehuda Bortz <yehudabortz@icloud.com>
2020-12-28 17:57:08 -0600

    commit tree ac0e24aa94f826879a1da0e91f5158df5e97e4a0

--- app.rb
+++ app.rb
@@ -1,4 +1,25 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+    
+    get '/' do
+        "Hello World"
+    end
+    
+    get '/name' do
+        "My name is Yehuda"
+    end
+
+    get '/hometown' do
+        "My hometown is Saint Louis"
+    end
+
+    get '/favorite-song' do
+        "My favorite song is River Flows In You"
+    end
+
+    post '/test' do
+        "Just a test"
+    end
+
 end
